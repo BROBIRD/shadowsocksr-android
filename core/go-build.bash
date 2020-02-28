cb34768e9834dc803fff4e0a53ddcfad885b7453
BROBIRD <brobirdcn@gmail.com>
2020-02-29 00:38:33 +0800

    Test UPX

--- core/go-build.bash
+++ core/go-build.bash
@@ -1,4 +1,11 @@
 #!/bin/bash
+UPX_VERSION="3.96"
+
+apt-get install wget -y
+wget -N https://github.com/upx/upx/releases/download/v${UPX_VERSION}/upx-${UPX_VERSION}-amd64_linux.tar.xz
+tar -xvf upx-${UPX_VERSION}-amd64_linux.tar.xz
+UPX_PATH="$(pwd)/upx-${UPX_VERSION}-amd64_linux"
+chmod +x ${UPX_PATH}/upx
 
 [[ -z "${ANDROID_NDK_HOME}" ]] && ANDROID_NDK_HOME="${ANDROID_HOME}/ndk-bundle"
 TOOLCHAIN="$(find ${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/* -maxdepth 1 -type d -print -quit)/bin"
@@ -22,9 +29,10 @@ for i in "${!ABIS[@]}"; do
         CGO_ENABLED=1 CC="${TOOLCHAIN}/${CLANG_ARCHS[$i]}${MIN_API}-clang" \
         GOOS=android GOARCH=${GO_ARCHS[$i]} \
         go build -v -ldflags='-s -w' -o "${OUT_DIR}/unstripped" \
-    && "${TOOLCHAIN}/${STRIP_ARCHS[$i]}-strip" "${OUT_DIR}/unstripped" -o "${OUT_DIR}/${ABI}/${BIN}" \
+    && "${TOOLCHAIN}/${STRIP_ARCHS[$i]}-strip" "${OUT_DIR}/unstripped" -o "${OUT_DIR}/uncompressed" \
+    && "${UPX_PATH}/upx" --brute --android-shlib -o "${OUT_DIR}/${ABI}/${BIN}" "${OUT_DIR}/uncompressed" \
     || exit -1
-    rm "${OUT_DIR}/unstripped"
+    rm "${OUT_DIR}/unstripped" "${OUT_DIR}/uncompressed"
 done
 
 cd "$ROOT"
