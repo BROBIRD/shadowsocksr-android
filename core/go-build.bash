4f4a69399b9319981a6a3e63f3f010cc2d059f6a
BROBIRD <brobirdcn@gmail.com>
2020-02-29 01:04:00 +0800

    try fix strip

--- core/go-build.bash
+++ core/go-build.bash
@@ -1,23 +1,23 @@
 #!/bin/bash
 UPX_VERSION="3.96"
-
+ 
 apt-get install wget -y
 wget -N https://github.com/upx/upx/releases/download/v${UPX_VERSION}/upx-${UPX_VERSION}-amd64_linux.tar.xz
 tar -xvf upx-${UPX_VERSION}-amd64_linux.tar.xz
 UPX_PATH="$(pwd)/upx-${UPX_VERSION}-amd64_linux"
 chmod +x ${UPX_PATH}/upx
-
+ 
 [[ -z "${ANDROID_NDK_HOME}" ]] && ANDROID_NDK_HOME="${ANDROID_HOME}/ndk-bundle"
 TOOLCHAIN="$(find ${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/* -maxdepth 1 -type d -print -quit)/bin"
 ABIS=(armeabi-v7a arm64-v8a x86 x86_64)
 GO_ARCHS=('arm GOARM=7' arm64 386 amd64)
 CLANG_ARCHS=(armv7a-linux-androideabi aarch64-linux-android i686-linux-android x86_64-linux-android)
 STRIP_ARCHS=(arm-linux-androideabi aarch64-linux-android i686-linux-android x86_64-linux-android)
-
+ 
 MIN_API="$1"
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUT_DIR="$ROOT/build/go"
-
+ 
 cd "$ROOT/src/main/jni/overture/main"
 BIN="liboverture.so"
 for i in "${!ABIS[@]}"; do
@@ -29,10 +29,9 @@ for i in "${!ABIS[@]}"; do
         CGO_ENABLED=1 CC="${TOOLCHAIN}/${CLANG_ARCHS[$i]}${MIN_API}-clang" \
         GOOS=android GOARCH=${GO_ARCHS[$i]} \
         go build -v -ldflags='-s -w' -o "${OUT_DIR}/unstripped" \
-    && "${TOOLCHAIN}/${STRIP_ARCHS[$i]}-strip" "${OUT_DIR}/unstripped" -o "${OUT_DIR}/uncompressed" \
-    && "${UPX_PATH}/upx" --brute --android-shlib -o "${OUT_DIR}/${ABI}/${BIN}" "${OUT_DIR}/uncompressed" \
+    && "${UPX_PATH}/upx" --brute --android-shlib -o "${OUT_DIR}/${ABI}/${BIN}" "${OUT_DIR}/unstripped" \
     || exit -1
-    rm "${OUT_DIR}/unstripped" "${OUT_DIR}/uncompressed"
+    rm "${OUT_DIR}/unstripped"
 done
-
+ 
 cd "$ROOT"
